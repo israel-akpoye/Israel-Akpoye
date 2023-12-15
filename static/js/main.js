ddf30d7a6ea412b836b7ea2e4e695188fbbf55f7
Israel Akpoye <isdawadakpoye@gmail.com>
2023-12-15 02:35:25 -0800

    WIP: Make entire new changes.

---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+      
+//togglemode
+function toggleMode(){
+    document.body.classList.toggle('white-mode');
+};
+
+//toggle navigation
+function toggle() {
+   const toggleNav = document.getElementById("nav-links");
+   toggleNav.classList.toggle('active');
+
+};
+//togglemode
+function Toggleprofile(){
+    document.body.classList.toggle('toggle-profile');
+};
+
